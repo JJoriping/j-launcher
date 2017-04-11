6fa17b4caf806992940cbe358d917834af0e981b
JJoriping <op@jjo.kr>
2017-04-11 22:51:19 +0900

    changed logo path

--- main.js
+++ main.js
@@ -39,7 +39,7 @@ function main(){
 		title: `${V} - Now Loading`,
 		width: OPT['width'] || 900,
 		height: OPT['height'] || 600,
-		icon: `file://${__dirname}/views/img/logo.ico`
+		icon: `./views/img/logo.ico`
 	};
 
 	mainWindow = new BrowserWindow(winOpt);
